3d31f70ecbb412384ba90799413d099fee9d24cd
gyani <g.arya.mallick@gmail.com>
2023-04-12 17:34:30 +0530

    added solution to search a 2d matrix, leetcode #74

---
+++ search_a_2d_matrix_74/leetcode_74.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int n = matrix.size();
+        int m = matrix[0].size();
+        int low = 0;
+        int high = n * m - 1;
+        while(low <= high){
+            int mid = (high - low) / 2 + low;
+            int row = mid / m;
+            int col = mid % m;
+            if(matrix[row][col] == target) return true;
+            else if(matrix[row][col] < target) low = mid + 1;
+            else high = mid - 1;
+        }
+        return false;
+    }
+};
