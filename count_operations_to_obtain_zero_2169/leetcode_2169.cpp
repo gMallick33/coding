d0f30d8c58d4f33a9aee15d3c767f7af0ac80c68
gyani <g.arya.mallick@gmail.com>
2023-05-02 02:52:39 +0530

    added solution to - count operations to obtain zero, leetcode #2169

---
+++ count_operations_to_obtain_zero_2169/leetcode_2169.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int countOperations(int num1, int num2) {
+        if(num1 == 0 || num2 == 0) return 0;
+        int count = 0;
+        while(num1 != num2 && num1 != 0 && num2 != 0){
+            if(num1 <= num2) num2 -= num1;
+            else num1 -= num2;
+            count++;
+        }
+        return count + 1;
+    }
+};
