dcb4a047eab06f3b8000e77d24c64afd2ce9e346
gyani <g.arya.mallick@gmail.com>
2023-05-28 11:53:34 +0530

    added solution to - new 21 game, leetcode #837

---
+++ new_21_game_837/leetcode_837.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    double new21Game(int n, int k, int maxPts) {
+        if(k == 0) return 1;
+        if(n >= k - 1 + maxPts) return 1.0;
+        vector<double> dp(n + 1, 0.0);
+        double prob = 0.0;
+        double windowSum = 1.0;
+        dp[0] = 1.0;
+
+        for(int i = 1; i <= n; i++){
+            dp[i] = windowSum / maxPts;
+            if(i < k) windowSum += dp[i];
+            else prob += dp[i];
+            if(i >= maxPts) windowSum -= dp[i - maxPts];
+        }
+        return prob;
+    }
+};
